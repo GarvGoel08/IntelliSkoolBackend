3e3897f3e4f14c3cff7e4b983e588bdaf66e1abb
Garv Goel <garvgoel2927@gmail.com>
2025-04-17 18:02:34 +0530

    I genuinely hope CORS works now

--- index.js
+++ index.js
@@ -72,6 +72,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
+      "http://localhost:5173/",
       "https://ezinterview.vercel.app",
       "https://ezinterview.vercel.app/",
     ],
@@ -90,12 +91,6 @@ import User from "./models/Users.js";
 io.on("connection", (socket) => {
   socket.on("connect-room", async ({ roomID, userID }) => {
     socket.join(roomID);
-    const classRoom = await Class.findById(roomID);
-    if (!classRoom) {
-      socket.emit("class-not-found");
-      socket.disconnect();
-      return;
-    }
     socket.on("ping", async () => {
       User.findByIdAndUpdate(userID, { $inc: { attendanceMinutes: 1 } });
     });
